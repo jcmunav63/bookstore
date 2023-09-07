8feff34ec8e3c80b6550779d40f09fe293cdcd64
jcmunav63 <jcmunav63@gmail.com>
2023-09-06 20:21:46 -0600

    Edit styles to correct images & fonts; include books array in booksSLice & test functions.

--- src/components/Bookdetails.js
+++ src/components/Bookdetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice';
+import circle from '../assets/circle.png';
 
 const Bookdetails = ({ book }) => {
   const dispatch = useDispatch();
@@ -9,9 +10,9 @@ const Bookdetails = ({ book }) => {
     <>
       <div className="div1">
         <div className="bookdetails">
-          <span className="categorysize">{book.category}</span>
-          <span className="titlesize">{book.title}</span>
-          <span className="authorsize">
+          <span className="categorysize montserrat">{book.category}</span>
+          <span className="titlesize robotoslab">{book.title}</span>
+          <span className="authorsize robotoslab">
             {book.author}
           </span>
         </div>
@@ -29,8 +30,17 @@ const Bookdetails = ({ book }) => {
           <button type="button" className="lightbutton3">Edit</button>
         </div>
       </div>
+      <div className="div2">
+        <img className="circle" src={circle} alt="percentage" width="60px" height="60px" />
+        <div>
+          <p className="percentage montserrat">64%</p>
+          <p className="completed montserrat">Completed</p>
+        </div>
+      </div>
       <div className="div3">
-        <button type="button" className="buttonbook">Update progress</button>
+        <p className="chaptertext">CURRENT CHAPTER</p>
+        <p className="chapter">Chapter 17</p>
+        <button type="button" className="buttonbook robotoslab">UPDATE PROGRESS</button>
       </div>
     </>
   );
@@ -38,7 +48,7 @@ const Bookdetails = ({ book }) => {
 
 Bookdetails.propTypes = {
   book: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
     category: PropTypes.string,
