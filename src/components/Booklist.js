fed5caf41705bbf9cf32407ca718240ecc55900c
jcmunav63 <jcmunav63@gmail.com>
2023-09-13 11:37:02 -0600

    Edit store, slice, reducer, actions, Booklist & Bookform components.

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Bookdetails from './Bookdetails';
 import Bookform from './Bookform';
+import { fetchBooks } from '../redux/books/booksSlice';
 
-function Booklist() { // { objectBooks }
+const Booklist = () => { // { objectBooks }
+  const dispatch = useDispatch();
   const objectBooks = useSelector((state) => state.books);
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
   return (
     <div className="booklist">
       <div>
@@ -24,6 +31,6 @@ function Booklist() { // { objectBooks }
       <div className="bottom" />
     </div>
   );
-}
+};
 
 export default Booklist;
